599bd91adf367edb0b2fc430220de66cf24ffa7c
mi <miinx@yandex.ru>
2021-05-23 21:25:13 +0300

    feat: validate forms

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,53 @@ $(document).ready(function(){
 
 	toogleSlide ('.catalog-item__link');
 	toogleSlide ('.catalog-item__back');
+
+	// Modal
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+	});
+	$('.button_mini').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
+
+	function validareForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+		},
+			messages: {
+					name: {
+					required: "Пожалуйста, введите своё имя",
+					minlength: jQuery.validator.format("Введите {0} символа!")
+					},
+					phone: "Пожалуйста, введите свой номер телефона",
+					email: {
+					required: "Пожалуйста, введите свою почту",
+					email: "Неправильно введен адрес почты"
+					}
+			}
+		});
+	};
+
+validareForms('#consultation-form');
+validareForms('#consultation	form');
+validareForms('#order	form');
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
+
 });
